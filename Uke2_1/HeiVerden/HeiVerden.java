959ef1eafe8b3821a359dcde5ef0b42ec4bf0289
Lars Emil Knudsen <larsemil87@gmail.com>
2018-01-07 16:40:20 +0100

    Initiellt eksempel til første forelesning (Uke2_1)
    Det blir mest sansynlig noen utvidelser til denne i forelesningen, disse blir i så fall oppdatert i etterkant

---
+++ Uke2_1/HeiVerden/HeiVerden.java
@@ -0,0 +1,10 @@
+// Klassen HeiVerden (må ha samme navn som .java filen)
+public class HeiVerden {
+	
+	// main metoden. Det er her applikasjonen starter. Det må være én slik per applikasjon. 
+	public static void main(String[] args) {
+		// Printer ut teksten "Hei, Verden" til terminalen
+		System.out.println("Hei, verden!");	
+	}
+	
+}
