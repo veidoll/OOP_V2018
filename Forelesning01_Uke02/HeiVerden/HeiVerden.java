741fe27fe56983ca9be722111a6e1b1ce5374902
Lars Emil Knudsen <larsemil87@gmail.com>
2018-01-14 10:24:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- Forelesning01_Uke02/HeiVerden/HeiVerden.java
+++ Forelesning01_Uke02/HeiVerden/HeiVerden.java
@@ -1,4 +1,7 @@
-// Klassen HeiVerden (må ha samme navn som .java filen)
+/*
+ Klassen HeiVerden 
+ (må ha samme navn som .java filen)
+*/
 public class HeiVerden {
 	
 	// main metoden. Det er her applikasjonen starter. Det må være én slik per applikasjon. 
